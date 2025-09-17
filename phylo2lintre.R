50eb9a9c1764c255c542a7278fd7daa55aa33993
miltontan <mtanichthys@gmail.com>
2025-09-16 19:11:30 -0500

    Cleanup style in phylo2lintre.R

--- phylo2lintre.R
+++ phylo2lintre.R
@@ -8,9 +8,17 @@ phy2lin <- function(tree, filename) {
 ntips <- length(tree$tip.label)
 
 filetext <- c(
-  paste0("   ", ntips, " sequences"),
-  sapply(1:length(tree$tip.label), \(x) paste(x, tree$tip.label[x], sep = " ") ),
-  sapply(1:length(tree$edge.length),\(x) paste0(format(tree$edge[x,1], width = 3), " and ", format(tree$edge[x,2], width = 3), "        ", format(tree$edge.length)[x]))
+    paste0("   ", ntips, " sequences"),
+    sapply(1:length(tree$tip.label),
+      \(x) paste(x, tree$tip.label[x], sep = " ")
+    ),
+    sapply(1:length(tree$edge.length),\(x)
+        paste0(format(tree$edge[x,1], width = 3),
+        " and ",
+        format(tree$edge[x,2], width = 3), "        ",
+        format(tree$edge.length)[x])
+    )
 )
-write.table(filetext, file = filename, quote = FALSE, col.names = FALSE, row.names = FALSE)
+write.table(filetext, file = filename, quote = FALSE, col.names = FALSE, 
+  row.names = FALSE)
 }
